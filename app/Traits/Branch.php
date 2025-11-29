4dd4654a75f6786961f4d812691f945e3a862e80
hasib2019 <hasib.9437.hu@gmail.com>
2025-11-30 01:15:48 +0600

    update

--- app/Traits/Branch.php
+++ app/Traits/Branch.php
@@ -8,9 +8,9 @@ trait Branch {
 
     public static function bootBranch() {
         static::addGlobalScope('branch_id', function (Builder $builder) {
-            if (auth()->user()->user_type == 'user') {
+            if (auth()->check() && auth()->user()->user_type == 'user') {
                 return $builder->where('branch_id', auth()->user()->branch_id);
-            }else {
+            } else {
                 if (session('branch_id') != '') {
                     $branch_id = session('branch_id') == 'default' ? null : session('branch_id');
                     return $builder->where('branch_id', $branch_id);
